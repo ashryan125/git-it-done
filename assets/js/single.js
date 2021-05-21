7280885cf161ece81e980fb709fcc367f1b2425b
ashryan <20115553+ashryan125@users.noreply.github.com>
2021-05-20 20:37:40 -0400

    push repo issues to single repo page and add error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,14 +1,32 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if(repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+
+ 
+};
 
 var getRepoIssues = function (repo) {
-  console.log(repo);
+
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-  fetch(apiUrl).then(function (response) {
+  fetch(apiUrl).then(function(response) {
     // request was successful
     if (response.ok) {
-      response.json().then(function (data) {
+      response.json().then(function(data) {
         // pass response data to dom function
         displayIssues(data);
 
@@ -19,13 +37,13 @@ var getRepoIssues = function (repo) {
       });
     }
     else {
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 
 };
 
-var displayIssues = function (issues) {
+var displayIssues = function(issues) {
   // if repository has no issues
   if (issues.length === 0) {
     issueContainerEl.textContent = "This repo has no open issues!";
@@ -78,4 +96,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
